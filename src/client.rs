483395b2fc4ac00e58cb0080869d983a379f3e01
Thomas Maurin <thomas@maurin.io>
2018-06-27 01:18:01 +0200

    [Client] Pass the rest of the args as msg

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,9 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-pub fn run(port: i32) {
+pub fn run(msg: Vec<String>, port: i32) {
     let mut stream = TcpStream::connect(format!("localhost:{}", port)).unwrap();
-    stream.write_all(b"hello world").unwrap();
+    stream.write_all(msg.join(",").as_bytes()).unwrap();
     let mut response = String::new();
     stream.read_to_string(&mut response).unwrap();
     println!("{}", response);
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main() {
     let mut args = env::args().skip(1);
     let mode = args.next().expect("Missing argument");
     let port = 5432;
+    let msg: Vec<String> = args.collect();
     match mode.as_ref() {
         "server" => server::run(port),
-        "client" => client::run(port),
+        "client" => client::run(msg, port),
         _ => panic!("Unknown argument: {}", mode),
     };
 }
