eced5c3e3aa4b05b9eeea63e6f8f01d136c2e195
Thomas Maurin <thomas@maurin.io>
2018-06-27 01:45:11 +0200

    [Server] Add get/set functions

--- src/server.rs
+++ src/server.rs
@@ -4,11 +4,32 @@ use std::thread;
 use std::net::{TcpListener,TcpStream};
 use std::process::exit;
 
+struct Item {
+    key: String,
+    value: String,
+}
+
+fn set(params: Vec<&str>) {
+    let item = Item {key: params[0].to_string(), value: params[1].to_string()};
+    println!("set: {} {}", item.key, item.value);
+}
+
+fn get(key: &str) {
+    println!("get {}", key);
+}
+
 fn handle_client(mut stream: TcpStream) {
     println!("Got a connection: {:?}", stream);
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("{}", str::from_utf8(&buffer).unwrap());
+    let mut msg = str::from_utf8(&buffer).unwrap().split(",");
+    let action = msg.next().expect("Missing argument");
+    let params: Vec<&str> = msg.collect();
+    match action.as_ref() {
+        "set" => set(params),
+        "get" => get(params[0]),
+        _ => panic!("Unknown argument: {}", action),
+    };
     stream.write_all(b"done").unwrap();
     stream.flush().unwrap();
 }
